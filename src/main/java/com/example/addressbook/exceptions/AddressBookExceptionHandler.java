24d522a16239818127603a7fa2c2f8833db17550
Your Name <you@example.com>
2022-05-26 17:55:58 +0530

    created cutom exception class and exception handler

--- src/main/java/com/example/addressbook/exceptions/AddressBookExceptionHandler.java
+++ src/main/java/com/example/addressbook/exceptions/AddressBookExceptionHandler.java
@@ -23,4 +23,12 @@ public class AddressBookExceptionHandler {
         ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST request", errorMessage);
         return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(AddressBookException.class)
+    public ResponseEntity<ResponseDTO> handleEmployeePayrollException(
+            AddressBookException exception) {
+        ResponseDTO responseDTO = new ResponseDTO("Exception while REST Request",
+                exception.getMessage());
+        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
 }
--- src/main/java/com/example/addressbook/service/AddressBookService.java
+++ src/main/java/com/example/addressbook/service/AddressBookService.java
@@ -1,6 +1,7 @@
 package com.example.addressbook.service;
 
 import com.example.addressbook.dto.ContactDTO;
+import com.example.addressbook.exceptions.AddressBookException;
 import com.example.addressbook.model.Contact;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,8 @@ public class AddressBookService implements IAddressBookService {
 
     @Override
     public Contact getContactById(int contactId) {
-        return contactList.get(contactId-1);
+        return contactList.stream().filter(contact -> contact.getContactId() == contactId).findFirst()
+                .orElseThrow(() -> new AddressBookException("Contact not found"));
     }
 
     @Override
