5fe9fd1943a5e1a25d98d97d11fe05326d15ed29
Your Name <you@example.com>
2022-05-26 11:07:39 +0530

    Storing the addressbook data

--- src/main/java/com/example/addressbook/service/AddressBookService.java
+++ src/main/java/com/example/addressbook/service/AddressBookService.java
@@ -9,38 +9,43 @@ import java.util.List;
 
 @Service
 public class AddressBookService implements IAddressBookService {
+
+    List<Contact> contactList = new ArrayList<>();
     @Override
     public List<Contact> getContact() {
-        List<Contact> contactList = new ArrayList<>();
-        contactList.add(new Contact(1,
-                new ContactDTO("Saiprakash", "Beemari", "Ramnagar", "Telangana", "Karimnagar", "505001", "9676113169")));
-        System.out.println(contactList.toString());
 
         return contactList;
     }
 
     @Override
     public Contact getContactById(int contactId) {
-        Contact contact = new Contact(1,
-                new ContactDTO("Manoj", "Gadham", "Rampoor", "Telangana", "Karimnagar", "505002", "9676009988"));
-        return contact;
+        return contactList.get(contactId - 1);
     }
 
     @Override
     public Contact createContact(ContactDTO contactDTO) {
-        Contact contact = new Contact(1, contactDTO);
-        return contact;
+        List<Contact> contactData = this.getContact();
+        contactList.add( new Contact(contactData.size() + 1, contactDTO));
+        return contactList.get(contactList.size() - 1);
     }
 
     @Override
     public Contact updateContact(int contactId, ContactDTO contactDTO) {
-        Contact contact = new Contact(1, contactDTO);
+        Contact contact = this.getContactById(contactId);
+        contact.setFirstName(contactDTO.getFirstName());
+        contact.setLastName(contactDTO.getLastName());
+        contact.setAddress(contactDTO.getAddress());
+        contact.setState(contactDTO.getState());
+        contact.setCity(contactDTO.getCity());
+        contact.setZip(contactDTO.getZip());
+        contact.setPhone(contactDTO.getPhone());
+        contactList.set(contactId - 1, contact);
         return contact;
     }
 
     @Override
     public void deleteContact(int contactId) {
-        // TODO Auto-generated method stub
+        contactList.remove(contactId-1);
 
     }
 }
